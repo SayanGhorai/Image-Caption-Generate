3b0e6cc67be84e6e61bd93afe78b823f7bbffd9d
Sayan Ghorai <sayanghorai639.hitcseaiml2020@gmail.com>
2026-06-26 09:55:40 +0530

    Added translation fallback handling

--- src/translate.py
+++ src/translate.py
@@ -47,9 +47,14 @@ class BengaliTranslator:
         pass
 
     def translate_to_bengali(self, text):
-        bengali_text = GoogleTranslator(
-            source="en",
-            target="bn"
-        ).translate(text)
+        try:
+            bengali_text = GoogleTranslator(
+                source="en",
+                target="bn"
+            ).translate(text)
 
-        return bengali_text
+            return bengali_text
+
+        except Exception as e:
+            print("Translation Error:", e)
+            return "বাংলা অনুবাদ পাওয়া যায়নি"
